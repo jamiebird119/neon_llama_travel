6f710106f4c259a709eae3fcd18364d76c9ce974
jamiebird119 <jamie.bird119@gmail.com>
2021-05-05 15:53:14 +0000

    Edits to index and base.

--- home/views.py
+++ home/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
+import random
 
 # Create your views here.
 
 def home(request):
     template = 'index.html'
-    context={}
+    bg_no=random.randint(0,5)
+    context={"bg_no": bg_no}
     return render(request, template, context)
