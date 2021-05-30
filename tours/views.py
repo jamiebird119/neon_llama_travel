32cead07d7cefdc670863ef645a2d49e7e96dda7
jamiebird119 <jamie.bird119@gmail.com>
2021-05-30 16:30:51 +0000

    Hot fix for json decode bug on details view

--- tours/views.py
+++ tours/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render, get_object_or_404
 from .models import Tour, Country, Region, Category
-import json 
+import json
 from django.db.models import Q
 from django.conf import settings
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 import requests
 
 # Create your views here.
+
+
 def tour_search(request):
     qs = Tour.objects.all()
     region = None
@@ -25,9 +27,9 @@ def tour_search(request):
     if request.method == "POST":
         name = request.POST.get("name", None)
         country = request.POST.get("country", None)
-        trip_type = request.POST.get("trip_type",None)
+        trip_type = request.POST.get("trip_type", None)
         branded = request.POST.get("branded", None)
-        service_level = request.POST.get("service_level",None)
+        service_level = request.POST.get("service_level", None)
         physical_grading = request.POST.get("physical_grading", None)
         travel_style = request.POST.get("travel_style", None)
         activity = request.POST.get("activity", None)
@@ -36,43 +38,50 @@ def tour_search(request):
     if request.method == "GET":
         region = request.GET.get("region", None)
         country = request.GET.get("country", None)
-        trip_type = request.GET.get("trip_type",None)
-        branded = request.GET.get("branded",None)
-        service_level = request.GET.get("service_level",None)
+        trip_type = request.GET.get("trip_type", None)
+        branded = request.GET.get("branded", None)
+        service_level = request.GET.get("service_level", None)
         physical_grading = request.GET.get("physical_grading", None)
         travel_style = request.GET.get("travel_style", None)
         activity = request.GET.get("activity", None)
         page = request.GET.get('page', None)
 
     if trip_type:
-        query = Q(category__name__contains=trip_type)| Q(category__name__contains=trip_type)
+        query = Q(category__name__contains=trip_type) | Q(
+            category__name__contains=trip_type)
         qs = qs.filter(query).distinct()
     if branded:
-        query = Q(category__name__contains=branded)| Q(category__name__contains=branded)
+        query = Q(category__name__contains=branded) | Q(
+            category__name__contains=branded)
         qs = qs.filter(query).distinct()
     if activity:
-        query = Q(category__name__contains=activity)| Q(category__name__contains=activity)
+        query = Q(category__name__contains=activity) | Q(
+            category__name__contains=activity)
         qs = qs.filter(query).distinct()
     if service_level:
-        query = Q(category__name__contains=service_level)| Q(category__name__contains=service_level)
+        query = Q(category__name__contains=service_level) | Q(
+            category__name__contains=service_level)
         qs = qs.filter(query).distinct()
     if physical_grading:
-        query = Q(category__name__contains=physical_grading)| Q(category__name__contains=physical_grading)
+        query = Q(category__name__contains=physical_grading) | Q(
+            category__name__contains=physical_grading)
         qs = qs.filter(query).distinct()
     if travel_style:
-        query = Q(category__name__contains=travel_style)| Q(category__name__contains=travel_style)
+        query = Q(category__name__contains=travel_style) | Q(
+            category__name__contains=travel_style)
         qs = qs.filter(query).distinct()
     if region:
-        query = Q(region=region)| Q(region=region)
+        query = Q(region=region) | Q(region=region)
         qs = qs.filter(query).distinct()
     if country:
-        query = Q(start_country__name__contains=country)| Q(finish_country__name__contains=country)
+        query = Q(start_country__name__contains=country) | Q(
+            finish_country__name__contains=country)
         qs = qs.filter(query).distinct()
         if not region:
-                country_obj = get_object_or_404(Country, name=country)
-                region = country_obj.continent
+            country_obj = get_object_or_404(Country, name=country)
+            region = country_obj.continent
     if name:
-        query = Q(tour__name__icontains=name)| Q(tour__name__icontains=name)
+        query = Q(tour__name__icontains=name) | Q(tour__name__icontains=name)
         qs = qs.filter(query).distinct()
 
     if sortkey == 'name':
@@ -96,7 +105,7 @@ def tour_search(request):
         page = 1
     except EmptyPage:
         response = paginator.page(paginator.num_pages)
-    
+
     if page == no_of_pages:
         prev_p = int(page) - 1
         next_p = None
@@ -104,27 +113,27 @@ def tour_search(request):
         next_p = 2
         prev_p = None
     else:
-        next_p = int(page) + 1 
+        next_p = int(page) + 1
         prev_p = int(page) - 1
-    context={
+    context = {
         'activity': activity,
-        'region':region,
-        'country':country,
-        'name':name,
+        'region': region,
+        'country': country,
+        'name': name,
         'header': header,
         'tours': response,
         'responses_no': responses_no,
         'no_of_pages': no_of_pages,
         'next': next_p,
-        'prev' : prev_p,
+        'prev': prev_p,
         'page': page,
         'no_info': no_info,
-        'branded':branded,
+        'branded': branded,
         'service_level': service_level,
         'physical_grading': physical_grading,
         'travel_style': travel_style
     }
-    template ='search.html'
+    template = 'search.html'
     return render(request, template, context)
 
 
@@ -133,24 +142,56 @@ def tour_details(request, tour_id):
     images = {}
     details = {}
     itinerary = {}
-    for item in json.loads(tour.images):
-        images[item["type"]]= item["image_href"]
-    for item in tour.category.all():
-        details[item.category_type.name.lower().replace(" ", "_")]=item.name
-    for item in json.loads(tour.details):
-        if item["detail_type"]["label"].lower().replace(" ", "_").replace("'","") == "packing_list":
-            details[item["detail_type"]["label"].lower().replace(" ", "_").replace("'","")] = item["body"].split("\n")
-        else:
-            details[item["detail_type"]["label"].lower().replace(" ", "_").replace("'","")] = item["body"]
+    try:
+        for item in json.loads(tour.images):
+            print(item)
+            images[item["type"]] = item["image_href"]
+        for item in tour.category.all():
+            details[item.category_type.name.lower().replace(" ", "_")
+                    ] = item.name
+        for item in json.loads(tour.details):
+            if item["detail_type"]["label"].lower().replace(" ", "_").replace("'", "") == "packing_list":
+                details[item["detail_type"]["label"].lower().replace(
+                    " ", "_").replace("'", "")] = item["body"].split("\n")
+            else:
+                details[item["detail_type"]["label"].lower().replace(
+                    " ", "_").replace("'", "")] = item["body"]
 
-    for item in json.loads(tour.itinerary)[0]:
-        itinerary[item] = json.loads(tour.itinerary)[0][item]
-        
-    context={
-       'tour': tour,
-       "images": images,
-       "details": details,
-       "itinerary": itinerary
+        for item in json.loads(tour.itinerary)[0]:
+            itinerary[item] = json.loads(tour.itinerary)[0][item]
+            
+        context = {
+            'tour': tour,
+            "images": images,
+            "details": details,
+            "itinerary": itinerary
+        }
+        template = "tour_details.html"
+        return render(request, template, context)
+
+    except Exception as e:
+        for item in json.loads(json.loads(tour.images)):
+            print(item)
+            images[item["type"]] = item["image_href"]
+        for item in tour.category.all():
+            details[item.category_type.name.lower().replace(" ", "_")
+                    ] = item.name
+        for item in json.loads(json.loads(tour.details)):
+            if item["detail_type"]["label"].lower().replace(" ", "_").replace("'", "") == "packing_list":
+                details[item["detail_type"]["label"].lower().replace(
+                    " ", "_").replace("'", "")] = item["body"].split("\n")
+            else:
+                details[item["detail_type"]["label"].lower().replace(
+                    " ", "_").replace("'", "")] = item["body"]
+
+        for item in json.loads(json.loads(tour.itinerary))[0]:
+            itinerary[item] = json.loads(json.loads(tour.itinerary))[0][item]
+
+    context = {
+        'tour': tour,
+        "images": images,
+        "details": details,
+        "itinerary": itinerary
     }
-    template="tour_details.html"
-    return render(request,template, context)
+    template = "tour_details.html"
+    return render(request, template, context)
